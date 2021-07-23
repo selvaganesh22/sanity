09d5f45c357eb278b0e7635d321d147bde12cb33
Marius Lundgård <studio@mariuslundgard.com>
2021-07-23 14:31:36 +0200

    fix(base): fix document actions typings and export

--- packages/@sanity/base/src/index.ts
+++ packages/@sanity/base/src/index.ts
@@ -1,3 +1,14 @@
 export {default as DefaultRootComponent} from './components/DefaultRootComponent'
 export {UserStore} from './datastores/user/types'
 export * from './theme'
+
+// Export document actions typings
+export {
+  DocumentActionComponent,
+  ActionDescription as DocumentActionDescription,
+  DocumentActionConfirmDialogProps,
+  DocumentActionDialogProps,
+  DocumentActionModalDialogProps,
+  DocumentActionPopoverDialogProps,
+  DocumentActionProps,
+} from './actions/utils/types'
